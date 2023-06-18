424a1cf9a30f7880974c44af90aa0dbf7ccb4bb8
Swetanshu Pandey <swetanshupandey1722@gmail.com>
2023-06-18 12:49:46 +0530

    hi

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_requirements_list() -> List[str]:
     """
 
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        requirement_file.readlines()
+        requirement_file.readlines().remove("-e .")
 
 
 setup(
