5024168baa198e464d77736d1a3bea0cf19e60e5
Swetanshu Pandey <swetanshupandey1722@gmail.com>
2023-06-18 17:34:43 +0530

    first

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+import sys
 from housing.logger import logging
+from housing.exception import HousingException
 app = Flask(__name__)
 
 @app.route("/",methods = ['GET','POST'])
 def index():
-    logging.info("We are testing logging module")
+    try:
+        raise Exception("We are testing custom exception")
+    except Exception as e:
+        housing =  HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("We are testing logging module")
     return "Starting Machine Learning Project, CICD pipeline has been established"
 
 
