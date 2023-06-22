e12410c8d4fd13440c852dfb41f93ce4e6fb6d41
Swetanshu Pandey <swetanshupandey1722@gmail.com>
2023-06-22 12:52:11 +0530

    first

--- housing/component/data_ingestion.py
+++ housing/component/data_ingestion.py
@@ -11,20 +11,23 @@ class DataIngestion:
 
     def __init__(self,data_ingestion_config:DataIngestionConfig):
         try:
-            logging.info(f"{'= '*20}Data Ingestion log started.{'='.*20} ")
+            logging.info(f"{'= '*20}Data Ingestion log started.{'= '*20}")
             self.data_ingestion_config = data_ingestion_config
         except Exception as e:
             raise HousingException(e,sys)
     
 
     def download_housing_data(self):
-         pass
+        try:
+            download_url =  self.data_ingestion_config.dataset_download_url
+            
+        except Exception as e:
     
     def extract_tgz_file(self):
-         pass
+        pass
     
     def split_data_as_train_test(self):
-         pass
+        pass
 
     def initiate_data_ingestion(Self) -> DataIngestionArtifact:
         try:
