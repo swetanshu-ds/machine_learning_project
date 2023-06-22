d22168e7c414e29d6e425ae6ec9e4c3704859d00
Swetanshu Pandey <swetanshupandey1722@gmail.com>
2023-06-22 11:53:32 +0530

    first

--- housing/constant/__init__.py
+++ housing/constant/__init__.py
@@ -25,4 +25,3 @@ DATA_INGESTION_TGZ_DOWNLOAD_DIR_KEY = "tgz_download_dir"
 DATA_INGESTION_INGESTED_DIR_NAME_KEY = "ingested_dir"
 DATA_INGESTION_TRAIN_DIR_KEY = "ingested_train_dir"
 DATA_INGESTION_TEST_DIR_KEY = "ingested_test_dir"
-
