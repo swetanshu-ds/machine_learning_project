41eda7252407bd7ccb5b47ad7eeeab6604274a86
Swetanshu Pandey <swetanshupandey1722@gmail.com>
2023-06-23 11:12:08 +0530

    first

--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -3,13 +3,13 @@ import sys
 
 class HousingException(Exception):
     
-    def __init__(self,error_message:Exception,error_detail:sys):
+    def __init__(self, error_message:Exception,error_detail:sys):
         super().__init__(error_message)
-        self.error_message =  HousingException.get_detailed_error_message(
-                        error_message=error_message,
-                        error_detail=error_detail
-        )
-    
+        self.error_message=HousingException.get_detailed_error_message(error_message=error_message,
+                                                                       error_detail=error_detail
+                                                                        )
+
+
     @staticmethod
     def get_detailed_error_message(error_message:Exception,error_detail:sys)->str:
         """
@@ -27,9 +27,10 @@ class HousingException(Exception):
         error message: [{error_message}]
         """
         return error_message
-    
+
     def __str__(self):
         return self.error_message
-    
+
+
     def __repr__(self) -> str:
-        return HousingException.__name__str()
+        return HousingException.__name__.str()
